f31a79941cd5bedce8605bf59b05a4a091bf7dde
ParkGiraffe <op5321@soongsil.ac.kr>
2024-03-14 23:33:08 +0900

    모바일프로그래밍 : 2주차-1 수정

--- 2024-1/모바일프로그래밍/week2-1/bin/dart_study.dart
+++ 2024-1/모바일프로그래밍/week2-1/bin/dart_study.dart
@@ -2,5 +2,5 @@ import 'package:dart_study/dart_study.dart' as dart_study;
 
 void main() {
   print('Hello world: ${dart_study.calculate()}!');
-  print('Hello world!');
+  // print('Hello world!');
 }
