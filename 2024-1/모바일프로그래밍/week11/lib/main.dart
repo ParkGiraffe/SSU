07e352f1d0c80d3b46888d52ae77fe9482f0c30b
ParkGiraffe <op5321@soongsil.ac.kr>
2024-05-22 10:20:34 +0900

    모바일프로그래밍 : 12주차-1

--- 2024-1/모바일프로그래밍/week11/lib/main.dart
+++ 2024-1/모바일프로그래밍/week11/lib/main.dart
@@ -4,100 +4,38 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Color _bgColor = Colors.amber;
-  bool _isCreated = false;
-  Color _squareColor = Colors.red;
-
-  void _handleBgColor() {
-    setState(() {
-      _bgColor = _bgColor == Colors.amber ? Colors.white : Colors.amber;
-    });
-  }
-
-  void _handleCreatingSquare() {
-    setState(() {
-      _isCreated = !_isCreated;
-    });
-  }
-
-  void _handleSquareColor() {
-    setState(() {
-      _squareColor = _squareColor == Colors.red ? Colors.blue : Colors.red;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: GestureDetector(
-        onTap: _handleBgColor,
-        child: Scaffold(
-          backgroundColor: _bgColor,
-          body: Column(
-            children: [
-              Expanded(
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      _isCreated
-                          ? Square(
-                              color: _squareColor,
-                            )
-                          : const SizedBox.shrink(),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          ElevatedButton(
-                            onPressed: _handleCreatingSquare,
-                            child: Text(_isCreated ? '삭제' : '생성'),
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          ElevatedButton(
-                            onPressed: _isCreated ? _handleSquareColor : null,
-                            child: const Text('색변경'),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
+    return const MaterialApp(
+      home: Scaffold(
+        body: Counter(
+          initialNumber: 2,
         ),
       ),
     );
   }
 }
 
-class Square extends StatefulWidget {
-  final Color color;
-
-  const Square({
+class Counter extends StatefulWidget {
+  final int initialNumber;
+  const Counter({
     super.key,
-    required this.color,
+    required this.initialNumber,
   });
 
   @override
-  State<Square> createState() => _SquareState();
+  State<Counter> createState() => _CounterState();
 }
 
-class _SquareState extends State<Square> {
-  int counter = 0;
+class _CounterState extends State<Counter> {
+  int counter = 5;
+  // var counter = widget.initialNumber;
+
   void _increaseCounter() {
     setState(() {
-      print('Called setState!!');
       counter++;
     });
   }
@@ -105,50 +43,29 @@ class _SquareState extends State<Square> {
   @override
   void initState() {
     super.initState();
-    print('Called initState!!');
-  }
-
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    print('Called didChangeDependencies!!');
-  }
-
-  @override
-  void didUpdateWidget(covariant Square oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    print('Called didUpdateWidget!!');
-  }
-
-  @override
-  void deactivate() {
-    super.deactivate();
-    print('Called deactivate!!');
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    print('Called dispose!!');
+    counter = widget.initialNumber;
+    // widget object : Counter 본래 위젯에서 값을 받아오는 방법. 근데, widget은 인스턴스 멤버 초기화 과정에서는 사용불가능하다. 본래 위젯에 해당 state가 존재하는지를 아직 알 수 없는 상황이기 때문이다.
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      color: widget.color,
-      child: Center(
-        child: GestureDetector(
-          onTap: _increaseCounter,
-          child: Text(
-            '$counter',
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-            ),
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Click Count',
+            style: TextStyle(fontSize: 30),
           ),
-        ),
+          Text(
+            '$counter',
+            style: const TextStyle(fontSize: 30),
+          ),
+          ElevatedButton(
+            onPressed: _increaseCounter,
+            child: const Icon(Icons.add),
+          ),
+        ],
       ),
     );
   }
